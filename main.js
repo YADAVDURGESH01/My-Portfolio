f0366814ba31680c6d3301d98f8053fbb2add1ab
Durgesh Yadav <126358394+YADAVDURGESH01@users.noreply.github.com>
2025-04-19 03:41:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,4 @@
-// circle skill ////////////////////////////////////////////
-/*
-const circles = document.querySelectorAll('.circle');
-circles.forEach(elem=>{
-    var dots = elem.getAttribute("data-dots");
-    var marked = elem.getAttribute("data-percent");
-    var percent = Math.floor(dots * marked / 100);
-    var points = "";
-    var rotate = 360 / dots;
 
-
-    for(let i = 0; i < dots ; i++){
-        points += `<div class="points" style="--i:${i}; --rot: ${rotate}deg"></div>`;
-    }
-    elem.innerHTML = points;
-
-    const pointsMarked = elem.querySelectorAll('.points');
-    for(let i = 0; i < percent ; i++){
-        pointsMarked[i].classList.add('marked')
-    }
-})
-
-*/
 
 // mix it up portfolio sction
 var mixer = mixitup('.portfolio-gallery');
@@ -148,10 +126,3 @@ function easeInOutCubic(t, b, c, d) {
 	t -= 2;
 	return c/2*(t*t*t + 2) + b;
 };
-
-
-
-
- https://smtpjs.com/v3/smtp.js
-
-
